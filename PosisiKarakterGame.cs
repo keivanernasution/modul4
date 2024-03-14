6a6c5f040d7ff0a9c0337ec397718cf6f711cf78
keivanernasution <keivan.ernasution@gmail.com>
2024-03-14 11:07:05 +0700

    menambahkan class PosisiKarakterGame

---
+++ PosisiKarakterGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_1302210099
+{
+    internal class PosisiKarakterGame
+    {
+        public enum posisi
+        {
+            Berdiri, 
+            Jongkok,
+            Tengkurap,
+            Terbang,
+        }
+
+        public enum transisi
+        {
+            TombolW,
+            TombolS, 
+            TombolX,
+        }
+
+        public class 
+        {
+            if()
+        }
+    }
+}
